3408bb325db3ee16574bdb30c0dd553bb5be6254
ConchOD <conchuod@users.noreply.github.com>
2019-01-30 14:31:34 +0000

    2x2 network ready to test

---
+++ design_sources/NetworkADPLL.v
@@ -0,0 +1,117 @@
+`timescale 1ns / 1ps
+
+module NetworkADPLL #(
+		parameter ACCUM_WIDTH = 12,
+		parameter PDET_WITH = 8,
+		parameter BIAS = 12'd76, //5 MHZ @ 258 MHZ CLOCK
+		parameter DCO_CC_WIDTH = 9,
+		//LoopFilter
+		parameter DYNAMIC_VAL = 0,
+		parameter ERROR_WIDTH = 8,
+		parameter KP_WIDTH = 3,
+		parameter KP_FRAC_WIDTH = 1,
+		parameter KP = 3'b010,
+		parameter KI_WIDTH = 4,
+		parameter KI_FRAC_WIDTH = 3,
+		parameter KI = 4'b0001,
+		//ErrorCombiner
+		parameter WEIGHT_WIDTH = 3
+	)
+	(
+        input wire reset_i,
+        input wire fpga_clk_i,		
+        input wire ref_left_i,
+        input wire ref_above_i,
+		input wire [PDET_WITH-1:0] error_right_i,
+		input wire [PDET_WITH-1:0] error_bottom_i,
+		input wire [KP_WIDTH-1:0] kp_i,
+		input wire [KI_WIDTH-1:0] ki_i,
+		input wire [WEIGHT_WIDTH-1:0] weight_left_i,
+		input wire [WEIGHT_WIDTH-1:0] weight_above_i,
+		input wire [WEIGHT_WIDTH-1:0] weight_right_i,
+		input wire [WEIGHT_WIDTH-1:0] weight_below_i,
+        output wire gen_clk_o,
+        output wire gen_div8_o,
+        output wire signed [PDET_WITH-1:0] error_left_o,
+        output wire signed [PDET_WITH-1:0] error_above_o,
+        output wire signed [DCO_CC_WIDTH-1:0] dco_cc_o
+    );
+	
+    localparam PADDING_WIDTH = ACCUM_WIDTH-DCO_CC_WIDTH;
+
+    wire [ACCUM_WIDTH-1:0] f_sel_sw_pa_x; //TODO
+    wire gen_clk_x;
+    wire gen_div8_x;
+    wire signed [PDET_WITH-1:0] error_x;
+
+    assign gen_clk_o = gen_clk_x;
+    assign gen_div8_o = gen_div8_x;
+    assign error_left_o = error_left_x;
+    assign error_above_o = error_above_x;
+	
+	
+	PhaseDetector #(.WIDTH(PDET_WITH)) pDetLeft (
+		.reset_i(reset_i), 
+		.fpga_clk_i(fpga_clk_i),
+		.reference_i(ref_left_i),
+		.generated_i(gen_div8_x),
+		.pd_clock_cycles_o(error_left_x)
+	);
+
+	PhaseDetector #(.WIDTH(PDET_WITH)) pDetAbove (
+		.reset_i(reset_i), 
+		.fpga_clk_i(fpga_clk_i),
+		.reference_i(ref_above_i),
+		.generated_i(gen_div8_x),
+		.pd_clock_cycles_o(error_above_x)
+	);
+	
+	ErrorCombiner errorCombiner ( //zero out unconnected, 2 weight on others
+		.reset_i(reset_x),
+        .weight_0_i(weight_above_i),
+        .weight_1_i(weight_left_i),
+        .weight_2_i(weight_right_i),
+        .weight_3_i(weight_below_i),
+        .error_0_i(error_above_x),
+        .error_3_i(error_left_x),
+        .error_1_i(error_right_i),        
+        .error_2_i(error_bottom_i),
+        .error_comb_o(error_x)
+	);
+    LoopFilter #(
+		.ERROR_WIDTH(ERROR_WIDTH),
+		.DCO_CC_WIDTH(DCO_CC_WIDTH),
+		.KP_WIDTH(KP_WIDTH),
+		.KP_FRAC_WIDTH(),
+		.KP(KP),
+		.KI_WIDTH(KI_WIDTH),
+		.KI_FRAC_WIDTH(KI_FRAC_WIDTH),
+		.KI(KI),
+		.DYNAMIC_VAL(DYNAMIC_VAL)	
+	)
+	loopFilter 
+	(
+        .gen_clk_i(gen_clk_x),
+        .reset_i(reset_i),
+        .error_i(error_x),
+        .kp_i(kp_i),
+        .ki_i(ki_i),
+        .dco_cc_o(dco_cc_o) 
+    );
+    PhaseAccum #(.WIDTH(ACCUM_WIDTH)) testOsc (
+        .enable_i(enable_i),
+        .reset_i(reset_i),
+        .fpga_clk_i(fpga_clk_i),
+        .clk_o(gen_clk_x),
+        .k_val_i(f_sel_sw_pa_x)
+	); 
+	Div8 div8 ( 
+		.reset_i(reset_i),
+    	.signal_i(gen_clk_x),
+    	.div8_o(gen_div8_x)
+   	);
+
+    //assign f_sel_sw_pa_x = BIAS;
+    assign f_sel_sw_pa_x = BIAS + $signed({ {(PADDING_WIDTH){dco_cc_o[DCO_CC_WIDTH-1]}} ,dco_cc_o});
+
+ endmodule // NetworkADPLL
---
+++ design_sources/TwoByTwoTest.v
@@ -0,0 +1,234 @@
+`timescale 1ns / 1ps
+
+module TwoByTwoTest (
+        input clk100_i,        // 100 MHz clock from oscillator on board
+        input rst_pbn_i,        // reset signal, active low, from CPU RESET pushbutton //
+        input [15:0] switches_i,
+        output [2:0] led_o,
+        output [6:0] ra_o,
+		input ra_i, // external reference
+        output [7:0] segment_o,
+        output [7:0] digit_o        
+    );
+
+    localparam ACCUM_WIDTH = 12;
+	localparam BIAS = 12'd154; //154 = 10 MHz
+	localparam PDET_WIDTH = 8;
+    //optimal gains => kp @ 1.4 = 1000 lower
+    //                 ki @ 1.8 = 1000 lower 
+    
+	wire reset_x;
+ 
+	wire ext_reference_x;
+    wire gen_reference_x;
+	
+    wire adpll_11_gen_x;
+    wire adpll_11_div8_x;
+    wire [PDET_WIDTH-1:0] adpll_11_error_top_x;
+    wire [PDET_WIDTH-1:0] adpll_11_error_left_x;
+
+    wire adpll_12_gen_x;
+    wire adpll_12_div8_x;
+    wire [PDET_WIDTH-1:0] adpll_12_error_top_x;
+    wire [PDET_WIDTH-1:0] adpll_12_error_left_x;
+
+    wire adpll_21_gen_x;
+    wire adpll_21_div8_x;
+    wire [PDET_WIDTH-1:0] adpll_21_error_top_x;
+    wire [PDET_WIDTH-1:0] adpll_21_error_left_x;
+
+    wire adpll_22_gen_x;
+    wire adpll_22_div8_x;
+    wire [PDET_WIDTH-1:0] adpll_22_error_top_x;
+    wire [PDET_WIDTH-1:0] adpll_22_error_left_x;
+
+    wire clk5_x;
+    wire clk5_0_x;
+    wire clk5_45_x;
+    wire clk5_90_x;
+    wire clk5_135_x;
+
+    wire clk258_x;
+
+    wire [ACCUM_WIDTH-1:0] ref_sel_c;
+
+    wire [3:0] kp_sel_x;
+    wire [3:0] ki_sel_x;
+    wire [7:0] kp_ki_c;
+
+    wire signed [7:0] ref_adpll_error_x;
+	wire signed [7:0] other_adpll_error_x;
+
+    wire [7:0] half_7seg_x;
+
+    assign ref_sel_c = 12'd36;
+	
+	assign ext_reference_x = ra_i;
+
+    assign kp_sel_x = switches_i[11:8];
+    assign ki_sel_x = switches_i[3:0];
+    assign kp_ki_c = {kp_sel_x,ki_sel_x};
+
+
+    ClockReset5_258_PDiff  clkGen  (
+        .clk100_i 	(clk100_i),      // input clock at 100 MHz
+        .rst_pbn_i 	(rst_pbn_i),     // input reset, active low
+        .clk5_0_o  	(clk5_0_x),
+        .clk5_45_o  (clk5_45_x),
+        .clk5_90_o  (),
+        .clk5_135_o (),
+        .clk258_o	(clk258_x),
+        .reset_o  	(reset_x)     	 // output reset, active high
+    );
+
+    assign clk5_x = clk5_0_x;
+    assign ra_o[0] = adpll_11_gen_x;
+    assign ra_o[1] = gen_reference_x;
+    assign ra_o[2] = ext_reference_x;
+    assign ra_o[4] = adpll_12_gen_x;
+    assign ra_o[5] = adpll_21_gen_x;
+    assign ra_o[6] = adpll_22_gen_x;
+
+    NetworkADPLL #(
+        .BIAS(BIAS),
+        .PDET_WITH(PDET_WIDTH),
+        //.KP(5'b00001),
+        .KP_WIDTH(5),
+        .KP_FRAC_WIDTH(4),
+        .KI_WIDTH(9),
+        .KI_FRAC_WIDTH(8),
+        //.KI(7'b0000001)
+        .DYNAMIC_VAL(1'b1) 
+    ) 
+    adpll_11
+    (
+        .reset_i(reset_x),
+        .fpga_clk_i(clk258_x),
+        .enable_i(switches_i[15]),
+        .error_right_i(~adpll_12_error_left_x), //adpll12
+        .error_bottom_i(~adpll_21_error_above_x), //adpll21
+        .ref_left_i(ext_reference_x), //reference
+        .ref_top_above_i(0'b0), //unused so just looping back
+        .weight_left_i(3'b001),
+        .weight_above_i(3'b000),
+        .weight_right_i(3'b001),
+        .weight_below_i(3'b001), 
+        .error_left_o(adpll_11_error_left_x),
+        .error_above_o(adpll_11_error_top_x),       
+        .gen_clk_o(adpll_11_gen_x),
+        .gen_div8_o(adpll_11_div8_x),
+        .kp_i(padded_kp_c), //padded_kp_c
+        .ki_i(padded_ki_c) //padded_ki_c
+    );
+
+    NetworkADPLL #(
+        .BIAS(BIAS+1),
+        .PDET_WITH(PDET_WIDTH),
+        //.KP(5'b00001),
+        .KP_WIDTH(5),
+        .KP_FRAC_WIDTH(4),
+        .KI_WIDTH(9),
+        .KI_FRAC_WIDTH(8),
+        //.KI(7'b0000001)
+        .DYNAMIC_VAL(1'b1) 
+    ) 
+    adpll_12
+    (
+        .reset_i(reset_x),
+        .fpga_clk_i(clk258_x),
+        .enable_i(switches_i[15]),
+        .error_right_i({(PDET_WIDTH){1'b0}}), //nothing
+        .error_bottom_i(~adpll_22_error_above_x), //adpll21
+        .ref_left_i(adpll_11_div8_x), //adpll11
+        .ref_top_above_i(1'b0), //unused so just looping back
+        .weight_left_i(3'b010),
+        .weight_above_i(3'b000),
+        .weight_right_i(3'b000),
+        .weight_below_i(3'b010), 
+        .error_left_o(adpll_12_error_left_x),
+        .error_above_o(adpll_12_error_top_x),       
+        .gen_clk_o(adpll_12_div8_x),
+        .gen_div8_o(adpll_12_gen_x),
+        .kp_i(padded_kp_c), //padded_kp_c
+        .ki_i(padded_ki_c) //padded_ki_c
+    );
+
+    NetworkADPLL #(
+        .BIAS(BIAS+2),
+        .PDET_WITH(PDET_WIDTH),
+        //.KP(5'b00001),
+        .KP_WIDTH(5),
+        .KP_FRAC_WIDTH(4),
+        .KI_WIDTH(9),
+        .KI_FRAC_WIDTH(8),
+        //.KI(7'b0000001)
+        .DYNAMIC_VAL(1'b1) 
+    ) 
+    adpll_21
+    (
+        .reset_i(reset_x),
+        .fpga_clk_i(clk258_x),
+        .enable_i(switches_i[15]),
+        .error_right_i(~adpll_22_error_left_x), //adpll12
+        .error_bottom_i({(PDET_WIDTH){1'b0}}), //nothing
+        .ref_left_i(1'b0), //nothing
+        .ref_top_above_i(adpll_11_div8_x),
+        .weight_left_i(3'b000),
+        .weight_above_i(3'b010),
+        .weight_right_i(3'b010),
+        .weight_below_i(3'b000), 
+        .error_left_o(adpll_21_error_left_x),
+        .error_above_o(adpll_21_error_top_x),       
+        .gen_clk_o(adpll_21_gen_x),
+        .gen_div8_o(adpll_21_div8_x),
+        .kp_i(padded_kp_c), //padded_kp_c
+        .ki_i(padded_ki_c) //padded_ki_c
+    );
+    NetworkADPLL #(
+        .BIAS(BIAS-1),
+        .PDET_WITH(PDET_WIDTH),
+        //.KP(5'b00001),
+        .KP_WIDTH(5),
+        .KP_FRAC_WIDTH(4),
+        .KI_WIDTH(9),
+        .KI_FRAC_WIDTH(8),
+        //.KI(7'b0000001)
+        .DYNAMIC_VAL(1'b1) 
+    ) 
+    adpll_22
+    (
+        .reset_i(reset_x),
+        .fpga_clk_i(clk258_x),
+        .enable_i(switches_i[15]),
+        .error_right_i({(PDET_WIDTH){1'b0}}), //adpll12
+        .error_bottom_i({(PDET_WIDTH){1'b0}}), //adpll21
+        .ref_left_i(adpll_21_div8_x), //reference
+        .ref_top_above_i(adpll_12_div8_x), //unused so just looping back
+        .weight_left_i(3'b010),
+        .weight_above_i(3'b010),
+        .weight_right_i(3'b000),
+        .weight_below_i(3'b000), 
+        .error_left_o(adpll_22_error_left_x),
+        .error_above_o(adpll_22_error_top_x),       
+        .gen_clk_o(adpll_22_gen_x),
+        .gen_div8_o(adpll_22_div8_x),
+        .kp_i(padded_kp_c), //padded_kp_c
+        .ki_i(padded_ki_c) //padded_ki_c
+    );
+
+    SignedDec2Hex sDec2Hex(
+        .signed_dec_i(kp_ki_c),
+        .hex_o(half_7seg_x)
+    );
+
+    //2s to unsigned to hex to seg lol
+    DisplayInterface disp1 (
+        .clock 		(clk5_0_x),       // 5 MHz clock signal
+        .reset 		(reset_x),      // reset signal, active high
+        .value 		(half_7seg_x),   // input value to be displayed
+        .point 		(4'b1111),    	// radix markers to be displayed
+        .digit 		(digit_o),      // digit outputs
+        .segment 	(segment_o)  	// segment outputs
+    );
+
+ endmodule // end of module TwoByTwoTest
