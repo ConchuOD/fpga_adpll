b2f2c90b27c4ddb4e57ee336786d00a2735cab5c
ConchOD <conchuod@users.noreply.github.com>
2019-02-25 16:19:43 +0000

    measurements

--- matlab/a1_3.m
+++ matlab/a1_3.m
@@ -1,4 +1,4 @@
-filename = 'test_measurement_number2.bin';
+filename = 'cdcd5.bin';
 [~,scope_y_data] = importAgilentBin(filename,1);
 [~,scope_y_data2] = importAgilentBin(filename,2);
 [~,scope_y_data3] = importAgilentBin(filename,3);
@@ -20,14 +20,41 @@ plot(t*1e6-7.95-0.09,scope_y_data4+0,'k');
 [~,init_cross3,~,~] = pulsewidth(scope_y_data3,scope_x_data, 'Polarity', 'Positive');
 [~,init_cross4,~,~] = pulsewidth(scope_y_data4,scope_x_data, 'Polarity', 'Positive');
 
+periods2 = getPeriods(init_cross2);
+periods3 = getPeriods(init_cross3);
+periods4 = getPeriods(init_cross4);
+
+std_period_2 = std(periods2);
+std_period_3 = std(periods3);
+std_period_4 = std(periods4);
+
+max_2 = max(periods2-mean(periods2));
+min_2 = min(periods2-mean(periods2));
+p2p_2 = max_2-min_2;
+max_3 = max(periods3-mean(periods3));
+min_3 = min(periods3-mean(periods3));
+p2p_3 = max_3-min_3;
+max_4 = max(periods4-mean(periods4));
+min_4 = min(periods4-mean(periods4));
+p2p_4 = max_4-min_4;
+
 diff2_3 = init_cross2-init_cross3;
 diff3_4 = init_cross3-init_cross4;
 diff4_2 = init_cross4-init_cross2;
 
-std2_3 = std(diff2_3);
-std3_4 = std(diff3_4);
-std4_2 = std(diff4_2);
+std_diff2_3 = std(diff2_3);
+std_diff3_4 = std(diff3_4);
+std_diff4_2 = std(diff4_2);
 
-mean2_3 = mean(abs(diff2_3));
-mean3_4 = mean(abs(diff3_4));
-mean4_2 = mean(abs(diff4_2));
+mean2_3 = mean((diff2_3));
+mean3_4 = mean((diff3_4));
+mean4_2 = mean((diff4_2));
+
+figure
+hold on;
+histfit(periods2)
+histfit(periods3)
+histfit(periods4)
+title('')
+xlabel('')
+ylabel('')
